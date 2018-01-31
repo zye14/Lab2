4722684891a6fcd923e7cdd7c86b0b5c6fe2f155
zye14 <zye14@illinois.edu>
2018-01-31 12:11:09 -0600

    Lab 2 finished

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,13 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            while (maze.isFinished() == false) {
+                maze.turnLeft();
+                while (maze.canMove() == false){
+                    maze.turnRight();
+                }
+                maze.move();
+            }
         }
 
         if (maze.isFinished()) {
